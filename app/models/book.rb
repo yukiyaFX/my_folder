1aca72779b16d18809277e2e06e654f4553734fc
yukiyaFX <kingyukiya@icloud.com>
2020-10-26 17:10:28 +0900

    a

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ApplicationRecord
-    validates :title, presence: { message: "error title must be given please" }
-    validates :body, presence: { message: "error body must be given please" }
+    validates :title, presence: true
+    validates :body, presence: true
 end
